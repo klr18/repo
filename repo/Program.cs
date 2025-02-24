f65afb8e69596ff1daae28db8e60416fac9527f0
Klara <clara.galieva7@gmail.com>
2025-02-24 12:00:35 +0300

    firstVersion

--- repo/Program.cs
+++ repo/Program.cs
@@ -22,46 +22,6 @@ class Program
 
             string choice = Console.ReadLine();
 
-            switch (choice)
-            {
-                case "1":
-                    Console.Write("Введите значение для A: ");
-                    A = Convert.ToDouble(Console.ReadLine());
-                    break;
-
-                case "2":
-                    Console.Write("Введите значение для B: ");
-                    B = Convert.ToDouble(Console.ReadLine());
-                    break;
-
-                case "3":
-                    Console.WriteLine($"Результат A + B: {A + B}");
-                    break;
-
-                case "4":
-                    Console.WriteLine($"Результат A - B: {A - B}");
-                    break;
-
-                case "5":
-                    Console.WriteLine($"Результат A * B: {A * B}");
-                    break;
-
-                case "6":
-                    if (B != 0)
-                        Console.WriteLine($"Результат A / B: {A / B}");
-                    else
-                        Console.WriteLine("Ошибка: деление на ноль.");
-                    break;
-
-                case "7":
-                    isRunning = false;
-                    break;
-
-                default:
-                    Console.WriteLine("Неверный выбор. Пожалуйста, выберите опцию от 1 до 7.");
-                    break;
-            }
-
             Console.WriteLine("\nНажмите любую клавишу для продолжения...");
             Console.ReadKey();
         }
